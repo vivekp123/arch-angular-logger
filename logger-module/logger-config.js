2f1b802024a1329183bcad4f4bf67f180c6d1bdf
angular-pdl <bapat.prakhar@gmail.com>
2015-03-26 14:44:50 +0530

    Update logger-config.js

--- logger-module/logger-config.js
+++ logger-module/logger-config.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('ngArchRef.logger', []).run(function(loggerService, LOGGER_OPTIONS, LOGGER_AJAX_OPTIONS, OfflineCache) {
+angular.module('myTmoApp.logger', []).run(function(loggerService, LOGGER_OPTIONS, LOGGER_AJAX_OPTIONS, OfflineCache) {
 
 	loggerService.setLoggerOptions(LOGGER_OPTIONS);
 
