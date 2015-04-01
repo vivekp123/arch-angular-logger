0e1371340d2f681dfa58275b4649d234dd2984bd
angular-pdl <bapat.prakhar@gmail.com>
2015-04-01 15:38:35 +0530

    Update logger-config.js

--- logger-module/logger-config.js
+++ logger-module/logger-config.js
@@ -1,15 +1,16 @@
 'use strict';
 
-angular.module('myTmoApp.logger', []).run(function(loggerService, LOGGER_OPTIONS, LOGGER_AJAX_OPTIONS) {
+angular.module('myTmoApp.logger', []).run(function (loggerService,LOGGER_OPTIONS, LOGGER_AJAX_OPTIONS, OfflineCache) {
 
 	loggerService.setLoggerOptions(LOGGER_OPTIONS);
-
+	
 	if(!navigator.onLine){
 		loggerService.detachAppender(LOGGER_AJAX_OPTIONS);
 		return;
 	}
-
+	
 	loggerService.setAJAXLoggerOptions(LOGGER_AJAX_OPTIONS);
-
+	
+	OfflineCache.sendLogsToServer();
 });
 
